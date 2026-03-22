a6412a1e5ea5765ffe80ba2474bdfee2350d1735
Taemur Baig <btaemur@yahoo.dk>
2026-03-22 17:19:21 +0100

    Updated script to accomodate choosing to compile with tagged stores

--- script.py
+++ script.py
@@ -11,12 +11,15 @@ if __name__ == "__main__":
     for ext in [".ll", ".opt.ll", ".o", ".bin", ".s"]:
         for file in pathlib.Path(__file__).parent.glob(f"*{ext}"):
             file.unlink()
-   
-    # Parse command-line arguments 
+
+    # Parse command-line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="input test file")
     parser.add_argument(
         "-a", "--assembly", help="generates assembly file", action="store_true"
+    )
+    parser.add_argument(
+        "-t", "--tagged", help="Uses tagged store instructions", action="store_true"
     )
     args = parser.parse_args()
 
@@ -55,19 +58,38 @@ if __name__ == "__main__":
         check=True,
     )
 
-    # .ll -> .opt.ll
-    print(f"Converting {input_no_ext}.ll to {input_no_ext}.opt.ll")
-    subprocess.run(
-        [
-            str(compiler_bin_dir / "opt"),
-            "-S",
-            "-passes=mem2reg",
-            str(base_dir / f"{input_no_ext}.ll"),
-            "-o",
-            str(base_dir / f"{input_no_ext}.opt.ll"),
-        ],
-        check=True,
-    )
+    # Tag store instructions if requested
+    if args.tagged:
+        # .ll -> .opt.ll
+        print(
+            f"Converting {input_file}.ll to {input_no_ext}.opt.ll with tagged store instructions"
+        )
+        subprocess.run(
+            [
+                str(compiler_bin_dir / "opt"),
+                "-S",
+                "-passes=mem2reg",
+                "-mattr=+tagged-mem-stores",
+                str(base_dir / f"{input_no_ext}.ll"),
+                "-o",
+                str(base_dir / f"{input_no_ext}.opt.ll"),
+            ],
+            check=True,
+        )
+    else:
+        # .ll -> .opt.ll
+        print(f"Converting {input_no_ext}.ll to {input_no_ext}.opt.ll")
+        subprocess.run(
+            [
+                str(compiler_bin_dir / "opt"),
+                "-S",
+                "-passes=mem2reg",
+                str(base_dir / f"{input_no_ext}.ll"),
+                "-o",
+                str(base_dir / f"{input_no_ext}.opt.ll"),
+            ],
+            check=True,
+        )
 
     # .opt.ll -> .o
     print(f"Converting {input_no_ext}.opt.ll to {input_no_ext}.o")
@@ -86,7 +108,7 @@ if __name__ == "__main__":
         ],
         check=True,
     )
-    
+
     # .o -> .bin
     print(f"Converting {input_no_ext}.o to {input_no_ext}.bin")
     subprocess.run(
@@ -116,5 +138,5 @@ if __name__ == "__main__":
                 "-o",
                 str(base_dir / f"{input_no_ext}.s"),
             ],
-            check=True
+            check=True,
         )
